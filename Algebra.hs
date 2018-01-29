649906a691e75cdf6ada38812e743e35086fa938
Ignat Insarov <kindaro@gmail.com>
2018-01-30 02:58:00 +0700

    Add variable lookup.

--- Algebra.hs
+++ Algebra.hs
@@ -6,8 +6,10 @@
 module Algebra where
 
 import Data.List
+import Data.Function (on)
+import Control.Monad.Trans.RWS.Strict
 
-data Expr a = Branch [a] | Leaf !Int | Bud !String deriving Show
+data Expr a = Branch [a] | Leaf !Int | Bud !String deriving (Eq, Show)
 
 instance Functor Expr where
     fmap f (Branch xs) = Branch (fmap f xs)
@@ -27,53 +29,74 @@ newtype Fix a = Fix { unFix :: a (Fix a) }
 instance Show (a (Fix a)) => Show (Fix a) where
     show (Fix x) = show x
 
+instance Eq (a (Fix a)) => Eq (Fix a) where
+    (Fix x) == (Fix y) = x == y
+
 branch = Fix . Branch
 leaf   = Fix . Leaf
 bud    = Fix . Bud
 
-evalSum :: Expr (Fix Expr) -> Fix Expr
-evalSum (Branch fxs) = floatSingleton . fmap Fix . concat $ separated
+type Eval = RWS [(String, Int)] [(Expr (Fix Expr), Expr (Fix Expr))] ()
+
+evalSum :: Expr (Fix Expr) -> Eval (Fix Expr)
+evalSum e@(Branch fxs) = me' >>= \e' ->
+    if e == e'
+        then return (Fix e)
+        else tell [(e, e')] >> return (Fix e')
+
   where
     xs :: [Expr (Fix Expr)]
     xs = unFix <$> fxs
 
-    splitted = groupBy (\x y -> evaluable x == evaluable y) xs 
+    me' :: Eval (Expr (Fix Expr))
+    me' = (floatSingleton . fmap Fix . concat) <$> separated
 
-    separated = case splitted of
-        [ ] -> [ ]
-        this@ ((x: _): ys) -> zipWith ($)
-                                ((if evaluable x then [eval] else [ ]) ++ cycle [id, eval])
-                                this
+    splitted :: Eval [[Expr (Fix Expr)]]
+    splitted = do
+        decorated <- sequence $ fmap (\x -> evaluable x >>= \flag -> return (x, flag)) xs
+        return $ (fmap.fmap) fst $ groupBy ((==) `on` snd) $ decorated 
 
-    eval :: [Expr a] -> [Expr a]
-    eval = pure . Leaf . eval'
+    separated :: Eval [[Expr (Fix Expr)]]
+    separated = splitted >>= \splitted ->
+        case splitted of
+            [ ] -> return [ ]
+            this@ ((x: _): ys) -> do
+                flag <- evaluable x
+                let evals = (if flag then [eval] else [ ]) ++ cycle [return, eval]
+                sequence $ zipWith ($) evals this
+                -- There is a guarantee that eval is never invoked on instances of Expr that are
+                -- not evaluable. Otherwise eval will error.
 
-    eval' :: [Expr a] -> Int
-    eval' [ ] = 0
-    eval' (Leaf i: xs) = i + eval' xs
-    eval'  x  = error $ "Algorithmic error: eval' can only evaluate leaves."
+    eval :: [Expr a] -> Eval [Expr a]
+    eval = fmap (pure . Leaf) . eval'
 
-    evaluable :: Expr a -> Bool
+    eval' :: [Expr a] -> Eval Int
+    eval' [ ] = return 0
+    eval' (Leaf i: xs) = eval' xs >>= return . (+i)
+    eval' (Bud  s: xs) = asks (lookup s) >>= \maybei ->
+        case maybei of
+            Nothing -> error $ "Algorithmic error: eval' cannot look the variable " ++ s ++ " up."
+            -- Justification for the error not occuring: separated only invokes eval on evaluable
+            -- instances of Expr.
+            Just i  -> eval' xs >>= return . (+i)
+
+    evaluable :: Expr a -> Eval Bool
     evaluable x = case x of
-        Branch xs -> False
-        Leaf   i  -> True
-        Bud    s  -> False
+        Branch xs -> return False
+        Leaf   i  -> return True
+        Bud    s  -> asks (lookup s) >>= \flag ->
+            case flag of
+               Just _  -> return True
+               Nothing -> return False
 
-evalSum x = Fix x
+evalSum x = return $ Fix x
 
-floatSingleton :: [Fix Expr] -> Fix Expr
-floatSingleton [x] = x
-floatSingleton  xs = Fix . Branch $ xs
+floatSingleton :: [Fix Expr] -> Expr (Fix Expr)
+floatSingleton [x] = unFix x
+floatSingleton  xs = Branch xs
 
 
 cata f = f . fmap (cata f) . unFix
-
--- |
--- λ cata evalSum $ branch [branch [leaf 1, leaf 2], leaf 3]
--- Leaf 6
---
--- λ cata evalSum $ branch [branch [leaf 1, leaf 2], leaf 4, branch [bud "x", leaf 8]]
--- Branch [Leaf 7,Branch [Bud "x",Leaf 8]]
 
 type AlgebraM a f b = a b -> f b
 
@@ -81,14 +104,5 @@ cataM :: (Monad f, Traversable a) => AlgebraM a f b -> Fix a -> f b
 cataM f x = f =<< (traverse (cataM f) . unFix $ x)
 
 -- |
--- λ let printAndReturn x = print x >> pure x
--- λ cataM (printAndReturn . evalSum) $ branch [branch [leaf 1, leaf 2], leaf 4, branch [bud "x", leaf 8]]
--- Leaf 1
--- Leaf 2
--- Leaf 3
--- Leaf 4
--- Bud "x"
--- Leaf 8
--- Branch [Bud "x",Leaf 8]
--- Branch [Leaf 7,Branch [Bud "x",Leaf 8]]
--- Branch [Leaf 7,Branch [Bud "x",Leaf 8]]
+-- λ runRWS (cataM evalSum $ branch [branch [leaf 1, leaf 2], leaf 4, branch [bud "x", leaf 8]]) [("x", 32)] ()
+-- (Leaf 47,(),[(Branch [Leaf 1,Leaf 2],Leaf 3),(Branch [Bud "x",Leaf 8],Leaf 40),(Branch [Leaf 3,Leaf 4,Leaf 40],Leaf 47)])
