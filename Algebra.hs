21544474baeaa1323f81a07300ee109dba54e470
Ignat Insarov <kindaro@gmail.com>
2018-01-29 11:04:29 +0700

    Enable doctest.

--- Algebra.hs
+++ Algebra.hs
@@ -23,6 +23,7 @@ evalSum (Leaf   i ) =     i
 
 cata f = f . fmap (cata f) . unFix
 
+-- |
 -- λ cata evalSum $ branch [branch [leaf 1, leaf 2], leaf 3]
 -- 6
 
@@ -31,6 +32,7 @@ type AlgebraM a f b = a b -> f b
 cataM :: (Monad f, Traversable a) => AlgebraM a f b -> Fix a -> f b
 cataM f x = f =<< (traverse (cataM f) . unFix $ x)
 
+-- |
 -- λ let printAndReturn x = print x >> pure x
 -- λ cataM (printAndReturn . evalSum) $ branch [branch [leaf 1, leaf 2], leaf 3]
 -- 1
